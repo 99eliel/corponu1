6aafce09651054eb2ff22d45e62290ebe868eab5
99eliel <131190282+99eliel@users.noreply.github.com>
2026-08-03 14:26:45 -0300

    release: carregar proteção rápida para todos os pagamentos 101

--- corponu-atualizador.js
+++ corponu-atualizador.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const LOCAL_RELEASE = "2026-08-03-registrar-chegada-rapido-100";
+  const LOCAL_RELEASE = "2026-08-03-todos-pagamentos-rapidos-101";
   const INTERVALO_VERIFICACAO = 60 * 1000;
   const RELOAD_KEY = "corponu_web_release_recarregada";
 
