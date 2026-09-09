696a951eb1ada0c225357e1e01e61fe98736491b
99eliel <131190282+99eliel@users.noreply.github.com>
2026-09-09 08:41:33 -0300

    fix: restaurar arvore exata anterior ao primeiro bloqueio

--- corponu-atualizador.js
+++ corponu-atualizador.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const LOCAL_RELEASE = "2026-09-09-pagamento-regularizado-release-287";
+  const LOCAL_RELEASE = "2026-09-04-faccoes-controlador-unificado-285";
   const INTERVALO_VERIFICACAO = 60 * 1000;
   const RELOAD_KEY = "corponu_web_release_recarregada";
 
